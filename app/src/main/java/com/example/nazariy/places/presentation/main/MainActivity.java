1da33b52fd571a4f912cc6f6bb2f6024bef2da17
nazariy <Nazarii_Moshenskyi@epam.com>
2018-04-17 02:46:04 +0300

    Added dependencies

--- app/src/main/java/com/example/nazariy/places/presentation/main/MainActivity.java
+++ app/src/main/java/com/example/nazariy/places/presentation/main/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.nazariy.places;
+package com.example.nazariy.places.presentation.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.nazariy.places.R;
 
 public class MainActivity extends AppCompatActivity {
 
